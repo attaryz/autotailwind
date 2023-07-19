805f4debd344951e2d7795ac869f71281ea335f5
attaryz <markosberg123@gmail.com>
2023-07-19 17:36:44 +0300

    init function + small restructure

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 #! /usr/bin/env node
 
 import inquirer from "inquirer"
-import { existing_project, new_project, remove_from_project } from "./scripts"
+// import chalk from "chalk"
 
+import { detect } from "./scripts/detect.js"
+import new_project from "./scripts/new_project.js"
+import existing_project from "./scripts/existing_project.js"
+import remove_from_project from "./scripts/remove_from_project.js"
+import chalk from "chalk"
+
+interface App {}
 // const program = new Command()
 
 // program
@@ -15,71 +22,99 @@ import { existing_project, new_project, remove_from_project } from "./scripts"
 
 // const options = program.opts()
 
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "type",
-      message: "What do you want to do?",
-      choices: [
-        "Create a new project",
-        "Add tailwind to an existing project",
-        "Remove tailwind from a project",
-        "Exit",
-      ],
-    },
-    {
-      type: "input",
-      name: "name",
-      message: "What is the name of your project?",
-      when: (answers) => answers.type === "Create a new project",
-    },
-    {
-      type: "input",
-      name: "path",
-      message: "Where do you want to create your project?",
-      when: (answers) => answers.type === "Create a new project",
-    },
-    {
-      type: "input",
-      name: "path",
-      message: "Where is your project?",
-      when: (answers) => answers.type === "Add tailwind to an existing project",
-
-      // validate: (value) => {
-      //   if (fs.existsSync(path.join(value, "package.json"))) {
-      //     return true
-      //   } else {
-      //     return "Please enter a valid path"
-      //   }
-      // },
-    },
-    {
-      type: "list",
-      name: "language",
-      message: "Where is your project?",
-      choices: ["TypeScript", "JavaScript"],
-      when: (answers) => answers.type === "Add tailwind to an existing project",
-    },
-    {
-      type: "input",
-      name: "path",
-      message: "Where is your project?",
-      when: (answers) => answers.type === "Remove tailwind from a project",
-    },
-  ])
-  .then((answers) => {
-    if (answers.type === "Create a new project") {
-      new_project(answers)
-    } else if (answers.type === "Add tailwind to an existing project") {
-      existing_project(answers)
-    } else if (answers.type === "Remove tailwind from a project") {
-      remove_from_project(answers)
-    } else {
-      process.exit()
-    }
-  })
-
 //
 
 // console.log(figlet.textSync("dd"))
+
+// const log = console.log
+
+// const errorLog = (message: string) => log(chalk.bold.red())
+// const successLog = (message: string) => log(chalk.bold.green())
+
+async function init() {
+  console.clear()
+
+  console.log(detect)
+
+  await inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "type",
+        message: "What do you want to do?",
+        choices: [
+          "Create a new project",
+          "Add tailwind to an existing project",
+          "Remove tailwind from a project",
+          "Exit",
+        ],
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of your project?",
+        when: (answers) => answers.type === "Create a new project",
+      },
+      {
+        type: "input",
+        name: "path",
+        message: "Where do you want to create your project?",
+        when: (answers) => answers.type === "Create a new project",
+      },
+      {
+        type: "input",
+        name: "path",
+        message: "Where is your project?",
+        when: (answers) =>
+          answers.type === "Add tailwind to an existing project",
+
+        validate: () => {
+          if (!detect.isDirty) {
+            return true
+          } else {
+            console.log(
+              chalk.bold.red("please commit your files then try again")
+            )
+            process.exit(0)
+          }
+        },
+
+        // validate: (value) => {
+        //   if (fs.existsSync(path.join(value, "package.json"))) {
+        //     return true
+        //   } else {
+        //     return "Please enter a valid path"
+        //   }
+        // },
+      },
+      {
+        type: "list",
+        name: "language",
+        message: "Where is your project?",
+        choices: ["TypeScript", "JavaScript"],
+        when: (answers) =>
+          answers.type === "Add tailwind to an existing project",
+      },
+      {
+        type: "input",
+        name: "path",
+        message: "Where is your project?",
+        when: (answers) => answers.type === "Remove tailwind from a project",
+      },
+    ])
+    .then((answers) => {
+      if (answers.type === "Create a new project") {
+        new_project(answers)
+      } else if (answers.type === "Add tailwind to an existing project") {
+        existing_project(answers)
+      } else if (answers.type === "Remove tailwind from a project") {
+        remove_from_project(answers)
+      } else {
+        process.exit()
+      }
+    })
+
+  // process.exit(0)
+}
+
+init()
--- src/scripts/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as existing_project } from "./existing_project"
-
-export { default as remove_from_project } from "./remove_from_project"
-
-export { default as new_project } from "./new_project"
